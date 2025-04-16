ef5c85bd0241aa8a77c1a228c64a80d83e63fd21
Tu Nombre <usuario@ejemplo.com>
2025-04-16 20:10:59 +0900

    Implementado exactamente el mismo código SPR que el experimento original

--- data_includes/main.js
+++ data_includes/main.js
@@ -286,12 +286,11 @@ Template("exercise.csv", row =>
   newTrial("exercise",
            newPrimer(),
            // Dashed sentence. Segmentation is marked by "*"
-           newController("DashedSentence", {
+           newController("SelfPacedReadingParadigmSentence", {
                s: row.SENTENCE, 
-               mode: "self-paced reading",
-               display: "dashed",
-               hideUnderscores: true,
-               splitRegex: /\*/
+               splitRegex: /\*/,
+               showAhead: false,
+               showBehind: false
            })
            .center()
            .print()
@@ -320,12 +319,11 @@ Template("experiment.csv", row =>
     newTrial( "experiment-"+row.TYPE,
               newPrimer(),
            // Dashed sentence. Segmentation is marked by "*"
-           newController("DashedSentence", {
+           newController("SelfPacedReadingParadigmSentence", {
                s: row.SENTENCE, 
-               mode: "self-paced reading",
-               display: "dashed",
-               hideUnderscores: true,
-               splitRegex: /\*/
+               splitRegex: /\*/,
+               showAhead: false,
+               showBehind: false
            })
            .center()
            .print()
